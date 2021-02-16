a3cb7c4a9e4d47a4611d18bd4bf52e9085cd24c3
montresor34 <8246292+montresor34@users.noreply.github.com>
2021-02-16 17:01:15 +0000

    Reorganise files; install react-router-dom

---
+++ littledata_test/src/components/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Littledata Welcome Flow Test</h1>
+    </div>
+  )
+}
+
+export default App
+
+//Needs router
+//3 buttons with paths to:
+//  John: <StoreInfo /> => <Survey /> => <EndScreen />
+//  Claire: <Survey /> => <EndScreen />
+//  Paul: <EndScreen />
