2d946b9689b5415cc6b8b867e4e180a8780416b6
montresor34 <8246292+montresor34@users.noreply.github.com>
2021-02-18 11:36:55 +0000

    Style Destination

--- littledata_test/src/components/Destination.js
+++ littledata_test/src/components/Destination.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import { Link }  from 'react-router-dom'
+
 import ga_logo from '../assets/ga_logo.svg'
 import segment_logo from '../assets/segment_logo.svg'
+import './Destination.css'
 
 function Destination() {
   return (
     <div className='destination__container'>
       <div className='destination__instruction'>
-        Please select the destination that you'd like to connet your store with
+        Please select the destination that you'd like to connect your store with
       </div>
       <div className='destination__btn-container'>
         <Link to='/end-screen'>
-          <div className='destination__btn'>
+          <div className='destination__btn google'>
             <img className='destination__btn-image' src={ga_logo} alt='Google Analytics logo'/>
-            <div className='destination__btn-text'>Google Analytics</div>
+            <div className='destination__btn-text'>Google<br />Analytics</div>
           </div>
         </Link>
         <Link to='/end-screen'>
-          <div className='destination__btn-image'>
-            <img src={segment_logo} alt='Segment logo' />
+          <div className='destination__btn segment'>
+            <img className='destination__btn-image' src={segment_logo} alt='Segment logo' />
             <div className='destination__btn-text'>Segment</div>
           </div>
         </Link>
